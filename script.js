7214705de82bed61591c650d7763d3f11650d6f1
Mani Deep <indlachinnu123@gmail.com>
2024-10-23 23:10:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+
+// Typewriter effect
+
+var typed = new Typed(".typewriter-text",
+    {
+        strings: [ "STUDENT", "WEB DEVELOPER", "WEB DESIGNER" ],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    }
+)
